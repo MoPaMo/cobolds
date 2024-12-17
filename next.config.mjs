390924d5aa0feedd2bc3fcc92fadc91a97cab892
MoPaMo <mopamo@users.noreply.github.com>
2024-12-17 21:20:46 +0100

    feat: add output setting to next.config.mjs and reorganize redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
+// cobolds/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-  async redirects() {
-    return [
-      {
-        source: "/learn",
-        destination: "/learn/introduction-1",
-        permanent: true,
-      },
-    ];
-  },
-};
-
-export default nextConfig;
+    output: 'export', 
+  
+    async redirects() {
+      return [
+        {
+          source: "/learn",
+          destination: "/learn/introduction-1",
+          permanent: true,
+        },
+      ];
+    },
+  
+  };
+  
+  export default nextConfig;
